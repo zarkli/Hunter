ffc53c5741a00b3b0e62d0fce45b4c9e34543c77
Leonardo Zarkli <vcatazrael@gmail.com>
2021-02-15 18:30:17 +0800

    natural

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   main.py
+@Time    :   2021/02/14 21:46:16
+@Author  :   Leonardo Zarkli 
+@Version :   0.1
+@Contact :   vcatazrael@gmail.com
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc    :   None
+'''
+
+# here put the import lib
+
---
+++ test/test1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test1.py
+@Time    :   2021/02/14 21:45:41
+@Author  :   Leonardo Zarkli 
+@Version :   0.1
+@Contact :   vcatazrael@gmail.com
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc    :   None
+'''
+
+# here put the import lib
