6936827408c851b880289b915ab56caf7d6e0e17
Leonardo Zarkli <vcatazrael@gmail.com>
2021-02-16 15:34:06 +0800

    normal

--- main.py
+++ main.py
@@ -11,4 +11,19 @@
 '''
 
 # here put the import lib
-import github3
+from abc import abstractclassmethod
+from re import DEBUG
+from github3 import login
+from github3.api import user
+
+def connect_to_github():
+    gh = login(username="zarkli",password="Mr.Zarkli@2003")
+    repo = gh.repository("zarkli","Hunter")
+    branch = repo.branch("master")
+
+    return gh,repo,branch
+
+
+
+if __name__ == '__main__':
+    connect_to_github()
